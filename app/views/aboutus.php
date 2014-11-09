2472c915383e3990577f6e168a9339a06f4ddca9
lusiaskolonie <lusiaskolonie@gmail.com>
2014-11-09 19:14:08 +0700

    about us

--- app/views/aboutus.php
+++ app/views/aboutus.php
@@ -11,8 +11,33 @@ window.location.href="#list";
 </style>
 <br/>
 <div class="col-sm-12">
-	<div class="col-sm-3">
-		<img src="<?php echo public_path().'/face221.png'?>">
+	<a href="https://www.facebook.com/pages/Mx1shop/857464980939365" style="text-decoration:none;color:#000;">
+	<div class="col-sm-4" align="center">
+	<p>
+		<img src="<?php echo $path;?>img/face221.png" width='100%'>
+	</p>
+	<p>
+		MX1shop
+	</p>
+	</div>
+	</a>
+
+	<div class="col-sm-4" align="center">
+	<p>
+		<img src="<?php echo $path;?>img/line221.png" width='100%'>
+	</p>
+	<p>
+		Line ID : mx1shop
+	</p>
+	</div>
+
+	<div class="col-sm-4" align="center">
+	<p>
+		<img src="<?php echo $path;?>img/tel2221.png" width='100%'>
+	</p>
+	<p>
+		Tel : 061- 410-3299 <br/>(ติดต่อ คุณธีรศักดิ์)
+	</p>
 	</div>
 </div>
 <div class="col-sm-12" style="height:200px;"></div>
