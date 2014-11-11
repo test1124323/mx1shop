70da009c0c99baad4403d57eca76cdbd7b57be09
kobpeapoo <kobpeapoo0425@gmail.com>
2014-11-11 20:30:34 +0700

    แก้ไขค้นหา

--- app/models/ProductModel.php
+++ app/models/ProductModel.php
@@ -25,8 +25,9 @@ class ProductModel extends Eloquent{
 		if(!empty($ProductName)){
 			$query->where('ProductName','LIKE','%'.$ProductName.'%');
 		}
-		if($CategoryID){
-			
+		if(!empty($CategoryID)){
+			$query->join('tbl_productcate', 'tbl_product.ProductID', '=', 'tbl_productcate.ProductID')
+			->where('tbl_productcate.CategoryID','=',$CategoryID);
 		}
 		
 		return $query;
