56c2739d1a76b642de16c526ea1be0ebb0e77f83
lusiaskolonie <lusiaskolonie@gmail.com>
2014-12-06 12:44:12 +0700

    dbblog

--- app/views/blogList.php
+++ app/views/blogList.php
@@ -11,11 +11,11 @@ window.location.href="#list";
 </style>
 <br/>
 <div class="col-sm-12">
+<a href="#list"><h2>บทความ</h2></a>
+<hr>
 <?php 
 foreach ($list as $key => $value) {
-	?>
-	<a href="#list"><h2>บทความ</h2></a>
-	<hr>
+	?>	
 	<!-- Array ( [0] => Array ( [TopicID] => 1 [TopicName] => ทดสอบบทความ [TopicDetail] => การทำงานที่ดี [vdo]http://mx1shop.com/public/backoffice/Topic/create[/vdo] ต้องรู้จักเคารพคนอื่น[vdo]https://www.youtube.com/watch?v=SWuRTOyzoRw[/vdo] [TopicNew] => 1 [TopicStatus] => 1 ) ) -->
 	<div class="blog-list"><a href="blog/<?php echo $value['TopicID']?>"><?php echo $value['TopicName'];?></a></div>
 	<?php
