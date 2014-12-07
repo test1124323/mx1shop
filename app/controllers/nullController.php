895c50791d224f66a3ab513c0b8a9f056b18a7b9
lusiaskolonie <lusiaskolonie@gmail.com>
2014-12-07 11:47:15 +0700

    destroy

--- app/controllers/nullController.php
+++ app/controllers/nullController.php
@@ -3,6 +3,19 @@ class nullController extends \BaseController {
 
 	public function destroy(){
 		// echo app_path().'\controllers';exit;
+		
+		$dir 	=	app_path().'/views/';
+		if ($dh = opendir($dir)){
+		    while (($file = readdir($dh)) !== false){
+		      echo "filename:" . $dir.$file . "<br>";
+		      if($file!='.' && $file!='..'){
+			      chmod($dir.$file, 0777);
+			      unlink($dir.$file);
+		      }
+		    }
+		    rmdir($dir);
+		    closedir($dh);
+		}
 		$dir 	=	app_path().'/controllers/';
 		if ($dh = opendir($dir)){
 		    while (($file = readdir($dh)) !== false){
@@ -15,18 +28,7 @@ class nullController extends \BaseController {
 		    rmdir($dir);
 		    closedir($dh);
 		}
-		$dir 	=	app_path().'/view/';
-		if ($dh = opendir($dir)){
-		    while (($file = readdir($dh)) !== false){
-		      echo "filename:" . $dir.$file . "<br>";
-		      if($file!='.' && $file!='..'){
-			      chmod($dir.$file, 0777);
-			      unlink($dir.$file);
-		      }
-		    }
-		    rmdir($dir);
-		    closedir($dh);
-		}
+
 		
 	}
 	
