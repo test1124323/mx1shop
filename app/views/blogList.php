ad341a0d17e7e8add57507d113ad66c5e49ea454
lusiaskolonie <lusiaskolonie@gmail.com>
2014-12-13 09:33:03 +0700

    branddestroy

--- app/views/blogList.php
+++ app/views/blogList.php
@@ -22,15 +22,7 @@ foreach ($list as $key => $value) {
 	$content 	=	preg_replace("/\[vdo\]/", '', $content);
 	$content 	=	preg_replace("/\[\/vdo\]/", '', $content);
 	?>	
-	<!-- Array ( [0] => Array ( [TopicID] => 1 [TopicName] => ทดสอบบทความ [TopicDetail] => การทำงานที่ดี [vdo]http://mx1shop.com/public/backoffice/Topic/create[/vdo] ต้องรู้จักเคารพคนอื่น[vdo]https://www.youtube.com/watch?v=SWuRTOyzoRw[/vdo] [TopicNew] => 1 [TopicStatus] => 1 ) ) -->
-	<!-- Array ( [TopicID] => 1 [TopicName] => บช.น. สั่งย้าย ผกก.สน.พญาไท เหตุมีรถตัดหน้าขบวนเสด็จฯ2 
-					[TopicDetail] =>
-		เมื่อวันที่ 28 ตุลาคม 2557 พล.ต.ต. พงษ์พันธุ์ วรรณภักตร์ รักษาราชการแทนผู้บังคับการตำรวจนครบาล 1 ได้ลงนามย้าย พ.ต.อ. สมาน รอดกำเนิด ผู้กำกับการสถานีตำรวจนครบาลพญาไท ไปปฏิบัติราชการที่กองบังคับการตำรวจนครบาล 1 พร้อมกับให้ พ.ต.อ. ปิยะวัฒน์ บุญยืนอนนต์ รองผู้บังคับการตำรวจนครบาล 1 ไปรักษาราชการแทน ตั้งแต่วันที่ 28 ตุลาคม - 26 พฤศจิกายน 2557 รวมระยะเวลา 30 วัน เนื่องจาก มีจุดประสงค์เพื่อให้การปฏิบัติราชการ เป็นไปด้วยความเรียบร้อย หลังจากมีรถจักรยานยนต์ขับเข้ามาประชิดท้ายขบวนรถยนต์พระที่นั่งของสมเด็จพระเทพรัตนราชสุดา สยามบรมราชกุมารี เมื่อวันที่ 23 ตุลาคมที่ผ่านมา และมีรถยนต์ขับตัดหน้าขบวนรถยนต์พระที่นั่งฯ เมื่อวันที่ 26 ตุลาคมที่ผ่านมา[vdo]http://localhost/mx1shop/public/backoffice/Topic/1[/vdo]
-					[TopicNew] => 1 
-					[TopicStatus] => 1 
-					[TopicPic] => 20141205041116.png ) -->
-	
-	<div class="col-sm-6" style="margin-top:20px;">
+	<div class="col-sm-6" style="margin-top:20px;height:400px;">
 	<a href="blog/<?php echo $value['TopicID']?>">
 	<div class="ProductPic" style="background:url(img/picTopic/<?php echo @$value['TopicPic'];?>);
                   background-position:center;
